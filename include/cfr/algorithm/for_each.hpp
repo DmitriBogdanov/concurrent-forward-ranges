41a18ce50c9152162bb05f4e1a7698dd42799576
DmitriBogdanov <dm.bogdanov2000@gmail.com>
2026-07-27 02:31:08 +0300

    feat: Implement `cfr::ranges::for_each`.

---
+++ include/cfr/algorithm/for_each.hpp
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 - present, Dmitri Bogdanov
+// SPDX-FileCopyrightText: https://github.com/DmitriBogdanov/concurrent-forward-ranges
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+// Content: Parallel standard algorithm.
+
+#include <algorithm>  // std::ranges::for_each, std::ranges::in_fun_result<>, std::ranges::borrowed_iterator_t<>, ...
+#include <functional> // std::ref()
+#include <utility>    // std::forward(), std::move()
+
+#include <cfr/backend/slice_for.hpp>      // cfr::ranges::slice_for
+#include <cfr/concept/parallel_range.hpp> // cfr::ranges::parallel_range
+
+namespace cfr::ranges {
+
+struct for_each_fn {
+    
+    template <class R, class F, class Proj = std::identity>
+        requires cfr::ranges::parallel_range<R> && std::indirectly_unary_invocable<F, std::projected<std::ranges::iterator_t<R>, Proj>>
+    constexpr auto operator()( R && range, F func, Proj proj = {} ) const
+        -> std::ranges::in_fun_result<std::ranges::borrowed_iterator_t<R>, F>
+    {
+        if consteval {
+            return std::ranges::for_each( std::forward<R>( range ), std::move( func ), std::move( proj ) );
+        }
+        else {
+            auto iter = cfr::ranges::slice_for( std::forward<R>( range ), [&] <class S> ( S && slice ) {
+                std::ranges::for_each( std::forward<S>( slice ), std::ref( func ), proj );
+            } ).in;
+            
+            return { std::move( iter ), std::move( func ) };
+        }
+    }
+    
+};
+
+inline constexpr auto for_each = for_each_fn{};
+
+} // namespace cfr::ranges
---
+++ tests/source/algorithm/for_each.cpp
@@ -0,0 +1,79 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026 - present, Dmitri Bogdanov
+// SPDX-FileCopyrightText: https://github.com/DmitriBogdanov/concurrent-forward-ranges
+//
+// SPDX-License-Identifier: MIT
+
+#include <common/fuzz_test.hpp>
+#include <common/unit_test.hpp>
+
+#include <array>  // std::array<>
+#include <deque>  // std::deque<>
+#include <list>   // std::list<>
+#include <vector> // std::vector<>
+
+#include <cfr/adaptor/adapt.hpp>     // cfr::views::adapt
+#include <cfr/adaptor/chunkable.hpp> // cfr::views::chunkable
+#include <cfr/adaptor/divisible.hpp> // cfr::views::divisible
+#include <cfr/algorithm/for_each.hpp> // cfr::ranges::for_each
+
+TEST_CASE( "cfr::ranges::for_each / compile-time evaluation" ) {
+    
+    constexpr auto evaluate_checked = [] {
+        auto range = std::array{ 0, 1, 2, 3 };
+        
+        cfr::ranges::for_each( range, [&]( auto && value ) { value += 10; });
+        
+        return range;
+    };
+    
+    constexpr auto evaluate_correct = [] {
+        return std::array{ 10, 11, 12, 13 };
+    };
+    
+    STATIC_CHECK( evaluate_checked() == evaluate_correct() );
+    
+}
+
+namespace {
+
+struct callable {
+    std::size_t state = 0;
+    
+    void operator()( auto && value ) { 
+        value += 10; ut::atomic_increment( state );
+    }
+};
+
+template <cfr::ranges::bounded_range R, std::invocable<R> A>
+void verify( A adaptor ) {
+    
+    /* */ auto checked_range = R{  0,  1,  2,  3,  4,  5,  6,  7,  8,  9 };
+    const auto correct_range = R{ 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+    
+    const auto [iter, func] = cfr::ranges::for_each( adaptor( checked_range ), callable{} );
+    
+    CHECK( std::ranges::equal( checked_range, correct_range ) ); // result is correct
+    
+    CHECK( func.state == std::ranges::size( correct_range ) ); // functor state is accumulated
+    
+}
+
+} // namespace
+
+TEST_CASE( "cfr::ranges::for_each / fuzzing" ) {
+    
+    ut::fuzz_test( [] { verify<std::vector<int>>( std::views::all       ); } );
+    ut::fuzz_test( [] { verify<std::vector<int>>( cfr::views::adapt     ); } );
+    ut::fuzz_test( [] { verify<std::vector<int>>( cfr::views::chunkable ); } );
+    ut::fuzz_test( [] { verify<std::vector<int>>( cfr::views::divisible ); } );
+    
+    ut::fuzz_test( [] { verify<std:: deque<int>>( std::views::all       ); } );
+    ut::fuzz_test( [] { verify<std:: deque<int>>( cfr::views::adapt     ); } );
+    ut::fuzz_test( [] { verify<std:: deque<int>>( cfr::views::chunkable ); } );
+    ut::fuzz_test( [] { verify<std:: deque<int>>( cfr::views::divisible ); } );
+    
+    ut::fuzz_test( [] { verify<std::  list<int>>( cfr::views::adapt     ); } );
+    ut::fuzz_test( [] { verify<std::  list<int>>( cfr::views::chunkable ); } );
+    ut::fuzz_test( [] { verify<std::  list<int>>( cfr::views::divisible ); } );
+    
+}
