d22f778fc19ab77d0924976ae92164527fd56796
DmitriBogdanov <dm.bogdanov2000@gmail.com>
2026-07-30 02:39:01 +0300

    refactor: Remove redundant `consteval` case.

--- include/cfr/algorithm/for_each.hpp
+++ include/cfr/algorithm/for_each.hpp
@@ -12,27 +12,25 @@
 #include <utility>    // std::forward(), std::move()
 
 #include <cfr/backend/slice_for.hpp>      // cfr::ranges::slice_for
-#include <cfr/concept/parallel_range.hpp> // cfr::ranges::parallel_range
+#include <cfr/concept/parallel_range.hpp> // cfr::ranges::parallel_range<>
 
 namespace cfr::ranges {
 
+template <class R, class F, class Proj>
+concept for_each_suitable = std::indirectly_unary_invocable<F, std::projected<std::ranges::iterator_t<R>, Proj>>;
+    
 struct for_each_fn {
     
     template <class R, class F, class Proj = std::identity>
-        requires cfr::ranges::parallel_range<R> && std::indirectly_unary_invocable<F, std::projected<std::ranges::iterator_t<R>, Proj>>
+        requires cfr::ranges::parallel_range<R> && cfr::ranges::for_each_suitable<R, F, Proj>
     constexpr auto operator()( R && range, F func, Proj proj = {} ) const
         -> std::ranges::in_fun_result<std::ranges::borrowed_iterator_t<R>, F>
     {
-        if consteval {
-            return std::ranges::for_each( std::forward<R>( range ), std::move( func ), std::move( proj ) );
-        }
-        else {
-            auto iter = cfr::ranges::slice_for( std::forward<R>( range ), [&] <class S> ( S && slice ) {
-                std::ranges::for_each( std::forward<S>( slice ), std::ref( func ), proj );
-            } ).in;
-            
-            return { std::move( iter ), std::move( func ) };
-        }
+        auto iter = cfr::ranges::slice_for( std::forward<R>( range ), [&] <class S> ( S && slice ) {
+            std::ranges::for_each( std::forward<S>( slice ), std::ref( func ), proj );
+        } ).in;
+        
+        return { std::move( iter ), std::move( func ) };
     }
     
 };
