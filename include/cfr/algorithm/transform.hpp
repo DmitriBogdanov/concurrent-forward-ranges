e61bf4fe0bb84428efb9c5dba928e51787d2c7ea
DmitriBogdanov <dm.bogdanov2000@gmail.com>
2026-07-30 03:09:12 +0300

    test: Add tests for `cfr::ranges::transform`.

--- include/cfr/algorithm/transform.hpp
+++ include/cfr/algorithm/transform.hpp
@@ -24,17 +24,22 @@ struct transform_fn {
     
     template <class R, class O, class F, class Proj = std::identity>
         requires cfr::ranges::parallel_range<R> && cfr::ranges::transform_suitable<R, O, F, Proj>
-    constexpr auto operator()( R && range, O && result, F func, Proj proj ) const
+    constexpr auto operator()( R && range, O && result, F func, Proj proj = {} ) const
         -> std::ranges::in_out_result<std::ranges::borrowed_iterator_t<R>, std::ranges::borrowed_iterator_t<O>>
     {
         auto source_end = std::ranges::end( range  );
         auto target_end = std::ranges::end( result );
         
-        // Zip and subdivide 2 ranges in sync, this enables concurrent writing in general case. In case of 
+        // Zip and subdivide 2 ranges in sync, this enables concurrent writing in a general case. In case of 
         // random access the underlying behavior will be the same as in a classic offset-based implementation.
         cfr::ranges::for_each( std::views::zip( std::forward<R>( range ), std::forward<O>( result ) ),
             [&] ( auto && pair ) { std::get<1>( pair ) = std::invoke( func, std::invoke( proj, std::get<0>( pair ) ) ); }
         );
+        
+        // Note: To play it safe we prohibit ranges without random access, we could make zip range
+        //       subdividable implicitly using `cfr::ranges::adapt` and have parallel transform work
+        //       for all cases, but doing such implicit perf degradation seems risky, better let users
+        //       construct the zip + for_each combo themselves if they want to do this kind of iteration.
         
         return { std::move( source_end ), std::move( target_end ) };
     }
--- tests/source/algorithm/transform.cpp
+++ tests/source/algorithm/transform.cpp
@@ -8,22 +8,23 @@
 
 #include <array>  // std::array<>
 #include <deque>  // std::deque<>
-#include <list>   // std::list<>
 #include <vector> // std::vector<>
 
-#include <cfr/adaptor/adapt.hpp>     // cfr::views::adapt
-#include <cfr/adaptor/chunkable.hpp> // cfr::views::chunkable
-#include <cfr/adaptor/divisible.hpp> // cfr::views::divisible
-#include <cfr/algorithm/for_each.hpp> // cfr::ranges::for_each
+#include <cfr/adaptor/adapt.hpp>       // cfr::views::adapt
+#include <cfr/adaptor/chunkable.hpp>   // cfr::views::chunkable
+#include <cfr/adaptor/divisible.hpp>   // cfr::views::divisible
+#include <cfr/algorithm/transform.hpp> // cfr::ranges::transform
 
-TEST_CASE( "cfr::ranges::for_each / compile-time evaluation" ) {
+TEST_CASE( "cfr::ranges::transform / compile-time evaluation" ) {
     
     constexpr auto evaluate_checked = [] {
         auto range = std::array{ 0, 1, 2, 3 };
         
-        cfr::ranges::for_each( range, [&]( auto && value ) { value += 10; });
+        auto result = std::array{ 0, 0, 0, 0 };
         
-        return range;
+        cfr::ranges::transform( range, result, [&]( auto && value ) { return value + 10; } );
+        
+        return result;
     };
     
     constexpr auto evaluate_correct = [] {
@@ -40,7 +41,7 @@ struct callable {
     std::size_t state = 0;
     
     constexpr auto operator()( auto && value ) { 
-        value += 10; ut::atomic_increment( this->state );
+        return value + 10;
     }
 };
 
@@ -48,19 +49,21 @@ template <cfr::ranges::bounded_range R, std::invocable<R> A>
 void verify( A adaptor ) {
     
     /* */ auto checked_range = R{  0,  1,  2,  3,  4,  5,  6,  7,  8,  9 };
+    /* */ auto storage_range = R{  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 };
     const auto correct_range = R{ 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
     
-    const auto [iter, func] = cfr::ranges::for_each( adaptor( checked_range ), callable{} );
+    const auto [source_iter, target_iter] = cfr::ranges::transform( adaptor( checked_range ), adaptor( storage_range ), callable{} );
     
-    REQUIRE( std::ranges::equal( checked_range, correct_range ) ); // result is correct
+    REQUIRE( std::ranges::equal( storage_range, correct_range ) ); // result is correct
     
-    REQUIRE( func.state == std::ranges::size( correct_range ) ); // functor state is accumulated
+    REQUIRE( source_iter == std::ranges::end( checked_range ) );
+    REQUIRE( target_iter == std::ranges::end( storage_range ) );
     
 }
 
 } // namespace
 
-TEST_CASE( "cfr::ranges::for_each / fuzzing" ) {
+TEST_CASE( "cfr::ranges::transform / fuzzing" ) {
     
     ut::fuzz_test( [] { verify<std::vector<int>>( std::views::all       ); } );
     ut::fuzz_test( [] { verify<std::vector<int>>( cfr::views::adapt     ); } );
@@ -71,9 +74,5 @@ TEST_CASE( "cfr::ranges::for_each / fuzzing" ) {
     ut::fuzz_test( [] { verify<std:: deque<int>>( cfr::views::adapt     ); } );
     ut::fuzz_test( [] { verify<std:: deque<int>>( cfr::views::chunkable ); } );
     ut::fuzz_test( [] { verify<std:: deque<int>>( cfr::views::divisible ); } );
-    
-    ut::fuzz_test( [] { verify<std::  list<int>>( cfr::views::adapt     ); } );
-    ut::fuzz_test( [] { verify<std::  list<int>>( cfr::views::chunkable ); } );
-    ut::fuzz_test( [] { verify<std::  list<int>>( cfr::views::divisible ); } );
     
 }
